9f5486f39b997b3e0560e2d5248be973a7f249a7
Morgan Fong <fong.morgan@berkeley.edu>
2017-11-28 01:37:06 -0800

    added read ability from 2 sensors

--- i2cmux.py
+++ i2cmux.py
@@ -9,14 +9,25 @@ CONTROL_ADDRESS = 0x04
 
 device = I2C.Adafruit_I2C(TCA9548A_ADDRESS, 1, True)
 
-# Try writing an 8-bit select number
-device.write8(CONTROL_ADDRESS, 1<<6)
-print("TCA Port #6")
+def tca_select(i):
+  # Try writing an 8-bit select number
+  device.write8(CONTROL_ADDRESS, 1<<i)
 
-# Try color sensor's I2C address
-temp = Adafruit_TCS34725.TCS34725(address=TCS34725_ADDRESS, busnum=1)
+# Two color sensors attached to mux port 2 and 6
+tca_select(2)
+tcs2 = Adafruit_TCS34725.TCS34725(address=TCS34725_ADDRESS, busnum=1)
+tca_select(6)
+tcs6 = Adafruit_TCS34725.TCS34725(address=TCS34725_ADDRESS, busnum=1)
 
 while True:
-  r, g, b, _ = temp.get_raw_data()
-  print("R: {0}, G: {1}, B: {2}".format(r, g, b))
+  # Read from first sensor
+  tca_select(2)
+  r, g, b, _ = tcs2.get_raw_data()
+  print("I2C PORT 2 - R: {0}, G: {1}, B: {2}".format(r, g, b))
+  time.sleep(0.5)
+
+  # Read from second sensor
+  tca_select(6)
+  r, g, b, _ = tcs6.get_raw_data()
+  print("I2C PORT 6 - R: {0}, G: {1}, B: {2}".format(r, g, b))
   time.sleep(0.5)
